7b2428128dec85486aaeaef6581bc66ee0418dc1
lian.hao <lianh925@hotmail.com>
2017-09-24 18:51:37 +0800

    MVC模块:简单调用，需调整web框架

---
+++ transwarp/urls.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#-*-coding=utf-8-*-
+#urls.py
+'''
+'''
+
+from transwarp.web import get, view
+from transwarp.model import User, Blog, Comment
+
+@view('test_users.html')
+@get('/')
+def test_users():
+    users = User.find_all()
+    return dict(users=users)
+    
